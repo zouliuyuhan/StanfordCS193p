39be1056b4d3a758d47ebe5883c8da95db11f81b
zouliuyuhan <qtzguai@gmail.com>
2023-11-26 21:12:31 +0800

    add swift test demo

---
+++ day1-Lecture 1/day1-Lecture 1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  day1-Lecture 1
+//
+//  Created by 邹灿灿 on 2023/11/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ day1-Lecture 1/day1-Lecture 1/day1_Lecture_1App.swift
@@ -0,0 +1,17 @@
+//
+//  day1_Lecture_1App.swift
+//  day1-Lecture 1
+//
+//  Created by 邹灿灿 on 2023/11/26.
+//
+
+import SwiftUI
+
+@main
+struct day1_Lecture_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
